b97f81e7b48683177b5fa6122e8ba776b3f2193e
nishant <email.nishant.singh@gmail.com>
2020-07-19 22:17:28 +0530

    Basics Completed

--- basics/VariablesAndDataTypes.kt
+++ basics/VariablesAndDataTypes.kt
@@ -1,6 +1,6 @@
 package basics
 
-fun main(args: String) {
+fun main(args: Array<String>) {
     //
     val a: Int = 10
     /*
@@ -17,10 +17,10 @@ fun main(args: String) {
     val i = 10
 
     val x = if (i < 10) {
-        print("i is smaller than 10 as i=${i}")
+        println("i is smaller than 10 as i=${i}")
         10.00
     } else {
-        print("i is bigger than 10")
+        println("i is bigger than 10")
         "big"
     }
     /*
@@ -28,46 +28,62 @@ fun main(args: String) {
     x
     res16: kotlin.Any = 10.0
     */
+    loops()
+    collections()
 }
 
 fun loops() {
     for (i in 1..10)
-        print("i = ${i} ")
+        println("i = ${i} ")
 //FOR LOOP
     val a = arrayOf(1, 2, 3, 4)
+    println("-----FOR LOOP-----")
     for (i in a.indices) {
-        print(a[i])
+        println("value of array at position ${i} = " + a[i])
     }
 
 //WHILE LOOP
     var i = 1
+    println("-----WHILE LOOP-----")
     while (i < 10) {
-        print("i = ${i}")
+        println("i = ${i}")
         i++
     }
+
+//DO WHILE
+    var x = 0
+    println("-----DO WHILE LOOP-----")
+    do {
+        println("value of x = ${x} ")
+        x++
+    } while (x < 10)
 }
 
 fun collections() {
 //ARRAY
     val a = arrayOf(1, 2, 3, 4)
+    println("-----Array-----")
     for (i in a.indices) {
-        print(a[i])
+        println("array element at index ${i} = ${a[i]}\n")
     }
 //LIST
+    println("-----List-----")
     val immutableList = listOf(1, 2, 3)
     //immutableList.add() cant be used here as its only read
-    immutableList.get(0)
-    print("immutable list $immutableList")
+    println("immutableList value at index 0 = " + immutableList.get(0) + "\n")
+    println("immutable list $immutableList\n")
 
     val mutableList = mutableListOf(1, 2, 3)
     mutableList.add(3)
     mutableList.remove(0)
-    print("mutable list $mutableList")
+    println("mutable list $mutableList\n")
 //SET
+    println("-----Set-----")
     val immutableSet = setOf(1, 2, 3, 3)
-    print("immutable set $immutableSet")
+    print("immutable set $immutableSet\n")
 
     val mutableSet = mutableSetOf(1, 2, 3, 3)
+    print("mutable set = $mutableSet\n")
     mutableSet.add(4)
-    print("mutable set $mutableSet")
+    print("mutable set after adding an element $mutableSet\n")
 }
