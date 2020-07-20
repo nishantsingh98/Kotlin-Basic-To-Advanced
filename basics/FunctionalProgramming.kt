91ab9b43379346b63a3b843954bac88d0123f994
nishant <email.nishant.singh@gmail.com>
2020-07-20 23:47:21 +0530

    Lambda funtion --Single argument --Multiple Argument

--- basics/FunctionalProgramming.kt
+++ basics/FunctionalProgramming.kt
@@ -1,5 +1,15 @@
 package basics
 
-fun main(args:Array<String>){
-    //code goes here 
+fun main(args: Array<String>) {
+    //LAMBDA FUNCTIONS
+    lambda()
+}
+
+fun lambda() {
+    //lambda with single argument and single return type (Int) -> Int
+    val timesTwo: (Int) -> Int = { x: Int -> x * 2 } //or  val timesTwo = { x:Int -> x * 2 }
+    println(timesTwo(4))
+
+    //Lambda with 2 arguments (Int,Int) -> (Int)
+    val add: (Int,Int) -> (Int) = { x: Int, y: Int -> x + y }
 }
