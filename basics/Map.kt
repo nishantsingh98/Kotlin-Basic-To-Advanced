e65a6a9ea578638905cc853439876cab575bebb1
nishant <email.nishant.singh@gmail.com>
2020-07-21 23:56:49 +0530

    method to double an element in list using lambdas

--- basics/Map.kt
+++ basics/Map.kt
@@ -1,5 +1,14 @@
 package basics
 
-fun main(args: Array<String>){
-    //initial commit
+fun main(args: Array<String>) {
+    map()
+}
+
+fun map() {
+    val list=(1..10).toList()
+    println("My list is $list")
+
+    //maps every element to its respective value of lambda expression
+    val doubleValue =list.map { element -> element*2 }
+    println("Doubled value is $doubleValue")
 }
