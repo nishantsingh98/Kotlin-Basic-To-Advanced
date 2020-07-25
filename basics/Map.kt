75f0ad0c46f674211e61645094028f0beaff3d1d
nishant <email.nishant.singh@gmail.com>
2020-07-25 23:59:42 +0530

    map and flatmap implementations part 1

--- basics/Map.kt
+++ basics/Map.kt
@@ -3,13 +3,28 @@ package basics
 fun main(args: Array<String>) {
     //this is not a hashmap
     map()
+    //flatMap()
+}
+
+fun flatMap() {
+    val nestedlist = listOf(
+        (1..10).toList(),
+        (11..20).toList(),
+        (21..30).toList()
+    )
+
+    println("Nested list is $nestedlist")
+
 }
 
 fun map() {
-    val list=(1..10).toList()
+    val list = (1..10).toList()
     println("My list is $list")
 
     //maps every element to its respective value of lambda expression
-    val doubleValue =list.map { element -> element*2 }
+    var doubleValue = list.map { element -> element * 2 }
+
+    //the value is not yet stored 
+    list.map { it * 10 }
     println("Doubled value is $doubleValue")
 }
