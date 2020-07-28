81131e00daa4dce16b67ce32cbfd68bad7281f22
nishant <email.nishant.singh@gmail.com>
2020-07-28 23:54:37 +0530

    let() implementation
    Calls the specified function block with this value as its argument and returns its result.

--- basics/usefulFunctions.kt
+++ basics/usefulFunctions.kt
@@ -1,5 +1,24 @@
 package basics
 
 fun main(args: Array<String>) {
-//initial
+    // let
+    let()
+}
+
+fun let() {
+    /*
+    inline fun <T, R> T.let(block: (T) -> R): R
+    Calls the specified function block with this value as its argument and returns its result.
+    */
+
+    val str: String? = "ABCDEFG"
+    str?.let {
+        println(" my string is $str now lowercase: ${str.toLowerCase()}")
+        val a = 10
+    }
+    /*
+    this will give error
+    as a is used in let block and its life is valid in the let block only
+    */
+    //println(a)
 }
