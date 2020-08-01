ee07f9268993f540458923c2c6895f34e7ddb163
nishant <email.nishant.singh@gmail.com>
2020-08-01 17:05:59 +0530

    To remove ambiguity
    super<Class>.methodName()

--- OOP/intefaces.kt
+++ OOP/intefaces.kt
@@ -1,27 +1,44 @@
 package OOP
 
-//Only method and property declaration but no implementation 
+//Only method and property declaration but no implementation
 interface Cars {
     val fuelCapacity: String
     fun drive(): String
 }
 
-class Sedan : Cars {
+interface Bike {
+    val fuelCapacity: String
+    fun drive() {
+        print("Riding Bike ")
+    }
+
+}
+
+interface Cycle {
+    fun drive(){
+        print("Riding cycle ")
+    }
+}
+
+class Yamaha : Bike, Cycle {
     override val fuelCapacity = "100 L"
 
-    override fun drive() = "Driving a sedan car "
+    //Here ambiguity is removed with the help of super<Class>.methodName()
+    override fun drive() {
+        super<Bike>.drive()
+    }
 }
 
 class Sports : Cars {
     override val fuelCapacity = "40L"
-
     override fun drive() = "Driving a sports car "
-
 }
 
+//to remove ambiguity when multiple interfaces are implemented by the same class
+
 fun main() {
-    val sedan = Sedan()
-    println("${sedan.drive()}fuel capacity is ${sedan.fuelCapacity}")
+    val bike = Yamaha()
+    println("${bike.drive()}fuel capacity is ${bike.fuelCapacity}")
 
     val sports = Sports()
     println("${sports.drive()}fuel capacity is ${sports.fuelCapacity}")
