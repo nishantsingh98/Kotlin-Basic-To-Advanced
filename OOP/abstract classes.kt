db718074ddb38a6085158314bd9ea69323761777
nishant <email.nishant.singh@gmail.com>
2020-08-01 15:58:05 +0530

    Abstract class --it can't be instantiated --their methods Don't need to have default implementation or values --the methods used in child class need to be overridden

---
+++ OOP/abstract classes.kt
@@ -0,0 +1,18 @@
+package OOP
+
+import kotlin.math.pow
+
+//Abstract class can't be instantiated and their methods Don't need to have default implementation or values
+abstract class AbstractShape(val name: String) {
+    //no need to have a initial value or a function body here
+    abstract fun area(): Double
+}
+
+class AbstractCircle(name: String, val radius: Double) : Shape(name) {
+    override fun area() = Math.PI * radius.pow(2.0)
+}
+
+fun main() {
+    val smallCircle = AbstractCircle("Circle", 2.34)
+    println(smallCircle.area())
+}
